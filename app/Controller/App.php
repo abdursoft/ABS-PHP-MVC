888a0f8351438507bb20f08a37440fa3bd2b3b57
abdursoft <abdursoft@gmail.com>
2023-12-06 01:19:30 +0600

    page title updated

--- app/Controller/App.php
+++ app/Controller/App.php
@@ -10,7 +10,12 @@
 namespace App\Controller;
 
 class App extends Controller{
+    public function __construct()
+    {
+        parent::__construct();
+    }
     public function index(){
+        $this->load->page_title = "Have a good journey with ABS framework";
         $this->load->view('welcome');
     }
     public function init(){
